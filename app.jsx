7dd08c4aa987e8a1d99b3fceb5a19ed84867ffff
DAWAR <dawarpratham8@gmail.com>
2025-11-27 18:49:24 +0530

    updated restaurant card photo

--- app.jsx
+++ app.jsx
@@ -20,7 +20,7 @@ const Heading= () =>{
 const RestaurantCard= () =>{
     return (
         <div className="restaurantCard">
-            <img src="https://via.placeholder.com/150" alt="Food Item" />
+            <img src="https://www.warmoven.in/cdn/shop/files/duel-delight-chocolate_-cake.jpg?v=1749833568&width=1080" alt="Food Item" />
             <h3>Food Item Name</h3>
             <p>Description of the food item.</p>
             <button>Add to Cart</button>
