9a3c62ed8ca223180232c272f1e16e00e53fbf7c
DAWAR <dawarpratham8@gmail.com>
2025-11-28 19:42:29 +0530

    subfolder in src for components

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Heading from "./Header.jsx";
-import RestaurantCard from "./RestaurantCard.jsx";
-import Body from "./Body.jsx";
-
-
+import Heading from "./components/Header.jsx";
+import RestaurantCard from "./components/RestaurantCard.jsx";
+import Body from "./components/Body.jsx";
 
 const FunctionalComponent= ()=>(
     <div className="appLayout">
         <Heading />
         <h2>Order your favourite food from here</h2>
-
         <Body />
-
-
     </div>
 );
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<FunctionalComponent />);
