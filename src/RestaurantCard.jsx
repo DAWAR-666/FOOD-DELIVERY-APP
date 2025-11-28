3bf141d183e3f40d37aab018cec69dbe269e37d4
DAWAR <dawarpratham8@gmail.com>
2025-11-28 19:38:56 +0530

    seperate file of resCard component

---
+++ src/RestaurantCard.jsx
@@ -0,0 +1,11 @@
+const RestaurantCard= () =>{
+    return (
+        <div className="restaurantCard">
+            <img src="https://www.warmoven.in/cdn/shop/files/duel-delight-chocolate_-cake.jpg?v=1749833568&width=1080" alt="Food Item" />
+            <h3>Food Item Name</h3>
+            <p>Description of the food item.</p>
+            <button>Add to Cart</button>
+        </div>
+    );
+}
+export default RestaurantCard;
--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Heading from "./Header.jsx";
-const RestaurantCard= () =>{
-    return (
-        <div className="restaurantCard">
-            <img src="https://www.warmoven.in/cdn/shop/files/duel-delight-chocolate_-cake.jpg?v=1749833568&width=1080" alt="Food Item" />
-            <h3>Food Item Name</h3>
-            <p>Description of the food item.</p>
-            <button>Add to Cart</button>
-        </div>
-    );
-}
+
 const Body= () =>{
     return (
         <div className="body"> 
