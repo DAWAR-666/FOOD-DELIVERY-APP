562b314aed9321a817271142e1b17cfeadcf8a5a
DAWAR <dawarpratham8@gmail.com>
2025-11-29 18:41:00 +0530

    destructured data in resCard.jsx

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,6 +1,7 @@
 import resData from "../utils/mockData.js";
 const RestaurantCard= (props) =>{
     const resData=props;
+    const {name,description,rating}=resData;
     return (
         <div className="restaurantCard">
             <img src="https://www.warmoven.in/cdn/shop/files/duel-delight-chocolate_-cake.jpg?v=1749833568&width=1080" alt="Food Item" />
