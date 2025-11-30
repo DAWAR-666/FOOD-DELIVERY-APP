7606d3404f9ef3800bc2cd9fcc14f152fba7dc1c
DAWAR <dawarpratham8@gmail.com>
2025-11-30 17:24:03 +0530

    added shimmer ui thingy

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,19 @@
 import RestaurantCard from "./RestaurantCard";
-import resData from "../utils/mockData";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 const Body= () =>{
     const [listOfRestaurants,setListOfRestaurants]=useState([]);
+    useEffect(()=>{
+     fecthData();
+    },[]);
     const fecthData=async ()=>{
         const data=await fetch ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json=await data.json();
         console.log(json);
-        setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    if (listOfRestaurants.length === 0) {
+        return <h1>Loading Restaurants...</h1>;
     }
     return (
         <div className="body"> 
@@ -15,13 +21,13 @@ const Body= () =>{
                 <input type="text" placeholder="Search for food items..." />
                 <button
                 onClick={()=>{
-                    const filteredList=listOfRestaurants.filter(res=>res.rating>4);
+                    const filteredList=listOfRestaurants.filter(res=>res.info.avgRating>4);
                     setListOfRestaurants(filteredList);
                 }}>top rating ({'>'}4)</button>
             </div>
             <div className="restaurantList">
                 {listOfRestaurants.map((restaurant)=> {
-                    return <RestaurantCard key={restaurant.id}res={restaurant} />
+                    return <RestaurantCard key={restaurant.info.id}res={restaurant} />
                 })}
                    
             </div>    
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -4,9 +4,9 @@ const RestaurantCard= (props) =>{
     return (
         <div className="restaurantCard">
             <img src="https://www.warmoven.in/cdn/shop/files/duel-delight-chocolate_-cake.jpg?v=1749833568&width=1080" alt="Food Item" />
-            <h3>{res.name}</h3>
+            <h3>{res.info.name}</h3>
             <p>{res.description}.</p>
-            <h4>rating:{res.rating}</h4>
+            <h4>rating:{res.info.avgRating}</h4>
         </div>
     );
 }
