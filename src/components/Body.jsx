33ea396aa32a9425c578856081373f2d8a63cdbe
DAWAR <dawarpratham8@gmail.com>
2025-11-30 16:31:28 +0530

    fetched res detail from api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,13 @@ import RestaurantCard from "./RestaurantCard";
 import resData from "../utils/mockData";
 import { useState } from "react";
 const Body= () =>{
-    const [listOfRestaurants,setListOfRestaurants]=useState(resData);
+    const [listOfRestaurants,setListOfRestaurants]=useState([]);
+    const fecthData=async ()=>{
+        const data=await fetch ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        console.log(json);
+        setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    }
     return (
         <div className="body"> 
             <div className="searchBar">
