83da9b24b123c03a2e73ddcca5fcbb8102cd8de2
DAWAR <dawarpratham8@gmail.com>
2025-12-01 20:17:20 +0530

    used ternary opr for shimmer ui

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,25 +3,29 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 const Body= () =>{
     const [listOfRestaurants,setListOfRestaurants]=useState([]);
+
+
     useEffect(()=>{
      fecthData();
     },[]);
+    
+    
     const fecthData=async ()=>{
         const data=await fetch ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json=await data.json();
         console.log(json);
         setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
-    if (listOfRestaurants.length === 0) {
-        return <h1>Loading Restaurants...</h1>;
-    }
-    return (
+    
+    
+    return listOfRestaurants.length === 0?
+    <h1>Loading Restaurants...</h1>:(
         <div className="body"> 
             <div className="searchBar">
                 <input type="text" placeholder="Search for food items..." />
                 <button
                 onClick={()=>{
-                    const filteredList=listOfRestaurants.filter(res=>res.info.avgRating>4);
+                    const filteredList=listOfRestaurants.filter(res=>res.info.avgRating>4.5);
                     setListOfRestaurants(filteredList);
                 }}>top rating ({'>'}4)</button>
             </div>
