bd684ada0caa9474b5e324f47f3a023cbb4fbc17
DAWAR <dawarpratham8@gmail.com>
2025-12-02 20:23:12 +0530

    created router path for about

--- src/app.jsx
+++ src/app.jsx
@@ -6,20 +6,24 @@ import Body from "./components/Body.jsx";
 import {createBrowserRouter ,RouterProvider} from "react-router-dom";
 
 const AppLayout= ()=>(
-    const router=createBrowserRouter([
-        {
-            path="/",
-            element:<AppLayout />,
-            errorElement:<Error />,
-        },
-        
-    ])
+    
     <div className="appLayout">
         <Heading />
         <h2>Order your favourite food from here</h2>
         <Body />
     </div>
 );
-
+const router=createBrowserRouter([
+        {
+            path:"/",
+            element:<AppLayout />,
+            errorElement:<Error />,
+        },
+        {
+            path:"about",
+            element:<About />
+        }
+        
+    ]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(< />);
