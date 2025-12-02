609b2211889187928c1d2fc06572abb5c2bbed5b
DAWAR <dawarpratham8@gmail.com>
2025-12-02 20:41:27 +0530

    imported useRouteError in Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,11 @@
+import { useRouteError } from "react-router-dom";
 const Error=()=>{
+    const err=useRouteError();
     return(
         <div>
             <h1>Oops! Page Not Found</h1>
             <p>The page you are looking for does not exist.</p>
-
+            <p>{err.status}:{err.statusText}</p>
         </div>
     );
 }
