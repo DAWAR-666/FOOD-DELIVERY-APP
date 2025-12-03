6ebe84c5254fe6ac6f46d186a32259b0d87b93cc
DAWAR <dawarpratham8@gmail.com>
2025-12-03 18:40:01 +0530

    created Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+const Contact=()=>{
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>If you have any questions, feedback, or need assistance, feel free to reach out to us!</p>
+            <ul>
+                <li>Email:</ li>
+                <li>Phone: </ li>
+                <li>Address: </ li>
+            </ul>
+            <p>We look forward to hearing from you!</p>
+        </div>
+    );
+}
