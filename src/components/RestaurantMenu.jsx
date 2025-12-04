dd7be50969f2568941d97a58df54d8c4b9221d50
DAWAR <dawarpratham8@gmail.com>
2025-12-04 20:59:20 +0530

    removed mock data file

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -12,14 +12,15 @@ const RestaurantMenu=()=>{
     
         const abcd=await fetch(url);
         const json=await abcd.json();
-        // console.log(json);
+        console.log(json);
         setMenuData(json?.data);
     }
     if(!menuData){
         return <h1>Loading Menu...</h1>;
     };
     const {name,avgRating,cuisines}=menuData?.cards[2]?.card?.card?.info;
-    const {items}=menuData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    const items=menuData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((e) =>
+    e.card?.card?.hasOwnProperty("itemCards"));
     console.log(menuData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((e) =>
     e.card?.card?.hasOwnProperty("itemCards")))
     return(
@@ -29,7 +30,15 @@ const RestaurantMenu=()=>{
             <h3>Average Rating: {avgRating}⭐</h3>
             <h2>Menu Items</h2>
             <ul>
-                
+                {items.map((e)=>(
+                    <li key={e.card.card.categoryId}> {e.card.card.itemCards.map((p)=>(
+
+                        <div key={p.card.info.id}>
+                            <h4>{p.card.info.name} - ₹{p.card.info.price/100 || p.card.info.defaultPrice/100}</h4>
+                            
+                        </div>
+                    ))}</li>
+                ))}
             </ul>
         </div>
     );
