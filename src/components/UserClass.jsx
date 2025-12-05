aa6b95749d5c22b9470e13a3fbce2ff179d2065a
DAWAR <dawarpratham8@gmail.com>
2025-12-05 23:25:11 +0530

    created a state var in userClass

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,12 +2,17 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            cnt1:1,
+        }
     };
+    
     render(){
         return(
             <div>
                 <h2>Class Component User</h2>
                 <p>This is a user profile rendered using a class component.</p>
+                cnt1: {this.state.cnt1}
                 <p>{this.props.name}</p>
             </div>
         );
