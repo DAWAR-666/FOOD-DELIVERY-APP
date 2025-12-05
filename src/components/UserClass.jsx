bc622f27e1e44cf260f06262eef75bdae9306718
DAWAR <dawarpratham8@gmail.com>
2025-12-05 23:25:56 +0530

    created multiple state var in userClass

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,6 +4,7 @@ class UserClass extends React.Component{
         super(props);
         this.state={
             cnt1:1,
+            cnt2:2
         }
     };
     
@@ -13,6 +14,9 @@ class UserClass extends React.Component{
                 <h2>Class Component User</h2>
                 <p>This is a user profile rendered using a class component.</p>
                 cnt1: {this.state.cnt1}
+                <br/>
+                cnt2: {this.state.cnt2}
+                <br/>
                 <p>{this.props.name}</p>
             </div>
         );
