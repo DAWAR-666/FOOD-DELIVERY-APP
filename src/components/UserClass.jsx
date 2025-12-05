7110a36591508e528f7f6f047cb430e4ca51e843
DAWAR <dawarpratham8@gmail.com>
2025-12-05 18:54:01 +0530

    defined component to access props

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 class UserClass extends React.Component{
+    constructor(props){
+        super(props);
+    };
     render(){
         return(
             <div>
                 <h2>Class Component User</h2>
                 <p>This is a user profile rendered using a class component.</p>
+                <p>{this.props.name}</p>
             </div>
         );
     }
