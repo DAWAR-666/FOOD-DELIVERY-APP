d517aecbd16314d6932d54f6ac1437ceb7860374
DAWAR <dawarpratham8@gmail.com>
2025-12-13 17:28:00 +0530

    added css to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,25 +3,25 @@ import { APP_LOGO_URL } from "../utils/Const";
 import useOnlineStatus from "../utils/useOnlineStatus";
 const Heading= () =>{
     return (
-        <div className="header">
-            <div className="Logo">
-                <img src={APP_LOGO_URL} alt="App Logo" />
+        <div className="flex items-center justify-between p-4 bg-amber-300 shadow-md ">
+            <div className="">
+                <img src={APP_LOGO_URL} alt="App Logo" className="h-32 rounded-xl"/>
             </div>
-            <h1>Food Delivery App</h1>
-            <ul>
-                <li>
+            <h1 className="text-4xl font-bold font-serif">Food Delivery App</h1>
+            <ul className="flex text-xl font-semibold">
+                <li className="m-3.5">
                     ONLINE: {useOnlineStatus() ? "✅" : "🔴"}
                 </li>
-                <li>
+                <li className="m-3.5">
                     <Link to="/">HOME</Link>
                 </li>
-                <li>
+                <li className="m-3.5">
                     <Link to="/about">ABOUT US</Link>
-                </li>
-                <li>
+                </li >
+                <li className="m-3.5">
                     <Link to="/contact">CONTACT US</Link>
                 </li>
-                <li>
+                <li className="m-3.5">
                     <button className="loginbtn">LOGIN</button>
                 </li>
             </ul>
