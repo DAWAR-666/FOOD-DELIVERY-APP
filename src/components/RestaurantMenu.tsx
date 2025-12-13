736efeedfa789024135e46f2e131baadd8f09176
DAWAR <dawarpratham8@gmail.com>
2025-12-13 18:16:01 +0530

    added css to RestaurantMenu

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -16,17 +16,19 @@ const RestaurantMenu=()=>{
     console.log(menuData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((e) =>
     e.card?.card?.hasOwnProperty("itemCards")))
     return(
-        <div className="menu">
-            <h1>{name}</h1>
-            <h3>Cuisine: {cuisines.join(", ")}</h3>
-            <h3>Average Rating: {avgRating}⭐</h3>
-            <h2>Menu Items</h2>
-            <ul>
+        <div className="p-4 m-4">
+            <div className="flex justify-center flex-col items-center mb-4">
+            <h1 className="text-4xl">{name}</h1>
+            <h3 className="text-lg">Cuisine: {cuisines.join(", ")}</h3>
+            <h3 className="text-lg">Average Rating: {avgRating}⭐</h3>
+            <h2 className="text-2xl">Menu Items</h2>
+            </div>
+            <ul className="p-2 text-lg ">
                 {items.map((e)=>(
-                    <li key={e.card.card.categoryId}> {e.card.card.itemCards.map((p)=>(
+                    <li  key={e.card.card.categoryId}> {e.card.card.itemCards.map((p)=>(
 
                         <div key={p.card.info.id}>
-                            <h4>{p.card.info.name} - ₹{p.card.info.price/100 || p.card.info.defaultPrice/100}</h4>
+                            <h4 >{p.card.info.name} - ₹{p.card.info.price/100 || p.card.info.defaultPrice/100}</h4>
                             
                         </div>
                     ))}</li>
