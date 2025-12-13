df7ad9aac200f306f4b437625cb4cfd6b8472de3
DAWAR <dawarpratham8@gmail.com>
2025-12-13 18:38:10 +0530

    added css to Contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,14 @@
 const Contact=()=>{
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>If you have any questions, feedback, or need assistance, feel free to reach out to us!</p>
-            <ul>
+        <div className="m-20 text-xl">
+            <h1 className="flex justify-center text-4xl">Contact Us</h1>
+            <p className='m-4'>If you have any questions, feedback, or need assistance, feel free to reach out to us!</p>
+            <ul className='m-4'>
                 <li>Email:</ li>
                 <li>Phone: </ li>
                 <li>Address: </ li>
             </ul>
-            <p>We look forward to hearing from you!</p>
+            <p className='m-4 flex justify-center'>We look forward to hearing from you!</p>
         </div>
     );
 }
