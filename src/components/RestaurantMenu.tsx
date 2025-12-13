1a9d00ebc8bc936b80f746cb73538e0b4bb4e5cf
DAWAR <dawarpratham8@gmail.com>
2025-12-13 18:45:22 +0530

    updated Menu css (list items)

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -28,7 +28,19 @@ const RestaurantMenu=()=>{
                     <li  key={e.card.card.categoryId}> {e.card.card.itemCards.map((p)=>(
 
                         <div key={p.card.info.id}>
-                            <h4 >{p.card.info.name} - ₹{p.card.info.price/100 || p.card.info.defaultPrice/100}</h4>
+                            <h4 className="flex 
+                                            justify-center 
+                                            border-amber-200 
+                                            border-2 
+                                            m-2 
+                                            p-2 
+                                            rounded-md 
+                                            hover:bg-amber-100 
+                                            hover:scale-105 
+                                            transition-transform 
+                                            duration-200">
+                                {p.card.info.name} - ₹{p.card.info.price/100 || p.card.info.defaultPrice/100}
+                                </h4>
                             
                         </div>
                     ))}</li>
