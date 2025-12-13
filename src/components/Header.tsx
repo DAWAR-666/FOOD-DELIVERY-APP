ff1892b9caddbc1d02fb7832c65ceb331fc2d884
DAWAR <dawarpratham8@gmail.com>
2025-12-13 18:59:22 +0530

    updated header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,45 @@ import useOnlineStatus from "../utils/useOnlineStatus";
 const Heading= () =>{
     return (
         <div className="flex items-center justify-between p-4 bg-[#ff6347] shadow-md text-white">
-            <div className="">
+            <div className="flex-1">
                 <img src={APP_LOGO_URL} alt="App Logo" className="h-32 rounded-xl"/>
             </div>
             <h1 className="text-4xl font-bold font-serif">Food Delivery App</h1>
-            <ul className="flex text-xl font-semibold cursor-pointer">
-                <li className="m-3.5  hover:text-gray-200 hover:underline">
-                    ONLINE: {useOnlineStatus() ? "✅" : "🔴"}
-                </li>
-                <li className="m-3.5  hover:text-gray-200 hover:underline">
-                    <Link to="/">HOME</Link>
-                </li>
-                <li className="m-3.5  hover:text-gray-200 hover:underline">
-                    <Link to="/about">ABOUT US</Link>
-                </li >
-                <li className="m-3.5  hover:text-gray-200 hover:underline">
-                    <Link to="/contact">CONTACT US</Link>
-                </li>
-                <li className="m-3.5  hover:text-gray-200 hover:underline">
-                    <button className="loginbtn">LOGIN</button>
-                </li>
-            </ul>
+            <div className="flex-1 flex justify-end">
+                <ul className="flex flex-1 text-lg font-semibold cursor-pointer ">
+                    <li className="m-2 ">
+                        ONLINE: {useOnlineStatus() ? "✅" : "🔴"}
+                    </li>
+                    <li className="m-2  
+                                    hover:text-gray-200 
+                                    hover:scale-105 
+                                    transition-transform 
+                                    duration-200">
+                        <Link to="/">HOME</Link>
+                    </li>
+                    <li className="m-2  
+                                    hover:text-gray-200 
+                                    hover:scale-105 
+                                    transition-transform 
+                                    duration-200">
+                        <Link to="/about">ABOUT US</Link>
+                    </li >
+                    <li className="m-2  
+                                    hover:text-gray-200 
+                                    hover:scale-105 
+                                    transition-transform 
+                                    duration-200">
+                        <Link to="/contact">CONTACT US</Link>
+                    </li>
+                    <li className="m-2  
+                                    hover:text-gray-200 
+                                    hover:scale-105 
+                                    transition-transform 
+                                    duration-200">
+                        <button className="loginbtn">LOGIN</button>
+                    </li>
+                </ul>
+            </div>
         </div>
         
     );
